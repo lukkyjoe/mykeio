c1d3ee73a4ffa241052dccb1351d17567612e228
Sawyer Schumacher <sawyer.schumacher@gmail.com>
2017-02-08 11:09:06 -0800

    add basic server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const path = require('path');
+const process = require('process');
+
+const app = express();
+app.use(express.static(path.join(__dirname , "app/dist/")));
+app.listen(process.env.port || 3000);
+
+
+

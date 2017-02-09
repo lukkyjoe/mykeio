13b796491b78a86b13a9aa358471ab2770c66f4b
Joseph <joseph.sc.luk@gmail.com>
2017-02-08 19:37:51 -0800

    Implement questioner component.

--- app/src/client/questioner.jsx
+++ app/src/client/questioner.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+export const DEFAULT_TEXT = 'Ask question';
+export const CLICKED_TEXT = 'Cancel';
+
 class Questioner extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       raisedHand: false
     }
+
+    this.handleClick = this.handleClick.bind(this);
   }
 
+  handleClick() {
+    this.setState(() => ({
+      raisedHand: !this.state.raisedHand
+    }));
+  }
 
   render() {
     return (
-      <div>This is from the questioner component!</div>
+        <button onClick={this.handleClick}>{this.state.raisedHand ? CLICKED_TEXT : DEFAULT_TEXT}</button>
     )
   }
 }

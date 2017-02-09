0cd1e124564fec903b21a6b7c92d2c9aa09aea94
Joseph <joseph.sc.luk@gmail.com>
2017-02-08 16:08:35 -0800

    Add lifecycle methods to test.

--- app/src/client/questioner.jsx
+++ app/src/client/questioner.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class Questioner extends React.Component {
-  componentDidMount() {
-
+  constructor(props) {
+    super(props)
+    this.state = {
+      raisedHand: false
+    }
   }
 
+  componentWillMount() {
+    console.log("component will mount");
+  }
+
+  componentDidMount() {
+    console.log("component did mount");
+  }
+
+  componentDidUpdate() {
+    console.log("component did update");
+  }  
+
   render() {
-    return ()
+    return (
+      <div></div>
+    )
   }
 }

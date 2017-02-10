730e62c537519381ca606a5a6908db8cc954c6c3
JonathanSchwob <jonathanrschwob@gmail.com>
2017-02-10 10:09:30 -0800

    add joinRoom func

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,23 +1,35 @@
 import React from 'react';
 import $ from 'jquery';
 
-class Main extends React.Component{
+class Main extends React.Component {
 
-  createNewRoom(){
+  createNewRoom() {
     $.get('/api/room/new')
       .done((data)=>{
-        window.location.href = "#/" + data.id
+        window.location.href = '#/' + data.id;
       })
       .fail(()=>{
         console.log('room creation failed');
-      })
+      });
   }
-  render(){
+
+  joinRoom(e) {
+    e.preventDefault();
+    console.log(e);
+    
+  }
+  render() {
     return (
       <div>
         <button onClick={this.createNewRoom}>Create New Room</button>
+        <br />
+        <form onSubmit={this.joinRoom}>
+          <input placeholder="Join Room"/>
+          <button>Join</button>
+        </form>
       </div>
-    )
+
+    );
   }
 }
 

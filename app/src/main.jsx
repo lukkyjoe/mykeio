97b3f8e9e83a063bf11cfcc13576eaa0e70013bc
Sawyer Schumacher <sawyer.schumacher@gmail.com>
2017-02-09 18:55:04 -0800

    add basic routing

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import $ from 'jquery';
 
-class Main extends React.component{
-  constructor(props) {
-    super(props);
+class Main extends React.Component{
+
+  createNewRoom(){
+    $.get('/api/room/new');
+  }
+
+  render(){
+    return (
+      <div>
+        <button onClick={this.createNewRoom}>Create New Room</button>
+      </div>
+    )
   }
 }
 

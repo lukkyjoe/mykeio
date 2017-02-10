d901ce9aee78e1a85a663bb814f5ac79932b5a74
Joseph <joseph.sc.luk@gmail.com>
2017-02-09 16:48:50 -0800

    Create question component.

---
+++ app/src/client/question.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Question = () =>
+  <div className="question">
+    <ul>
+      <li className="speech"></li>
+      <button>Accept</button>
+      <button>Reject</button>
+    </ul>
+  </div>
+  ;
+export default Question;

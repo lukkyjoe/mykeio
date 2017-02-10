efd12786ea9bc01f1e69728fac0809ed5e6626d6
Joseph <joseph.sc.luk@gmail.com>
2017-02-09 16:50:28 -0800

    Remove unnecessary comments.

--- app/src/client/speaker.test.js
+++ app/src/client/speaker.test.js
@@ -2,13 +2,6 @@ import React from 'react';
 import Speaker, {DEFAULT_TEXT, CLICKED_TEXT} from './speaker.jsx';
 import {shallow} from 'enzyme';
 
-// describe('The speaker component', () => {
-//     it('the speaker should show text', () => {
-//         const app  = shallow(<speaker/>);
-//         expect(app.contains(<div>This is from the speaker component!</div>)).toBe(true);
-//     })
-// })
-
 describe('The speaker component', () => {
     let speaker;
     beforeEach(() => {

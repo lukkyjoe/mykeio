f52b4af916a19598b467be911fe46c44d2cbdd52
Sawyer Schumacher <sawyer.schumacher@gmail.com>
2017-02-10 10:06:24 -0800

    added room linking and routes

--- app/src/room.jsx
+++ app/src/room.jsx
@@ -2,5 +2,11 @@ import React from 'react';
 import $ from 'jquery';
 
 class Room extends React.Component{
-
+  render(){
+    return (
+      <p>This is the room Component</p>
+    )
+  }
 }
+
+export default Room;
--- app/src/routes.jsx
+++ app/src/routes.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Main from './main.jsx'
+import Room from './room.jsx'
 import {render} from 'react-dom';
 import {Router, Route, Link, hashHistory} from 'react-router'; 
 
 render(
   <Router history = {hashHistory}>
     <Route path='/' component={Main}/>
+    <Route path='/:roomid' component={Room}/>
   </Router>
   , document.getElementById('app'));

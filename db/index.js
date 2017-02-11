b1a94f2e729613c3b51ddf0cb7c8856f8f0be9c6
Joseph <joseph.sc.luk@gmail.com>
2017-02-10 19:56:54 -0800

    Refine schema and models.

--- db/index.js
+++ db/index.js
@@ -2,23 +2,25 @@
 const mongoose = require('mongoose');
 const mongoUri = 'mongodb://localhost/myke';
 const Schema = mongoose.Schema;
+const db = mongoose.connect(mongoUri);
 
-const Quizzes = mongoose.model('Quizzes', quizSchema);
-
-let quizSchema = new Schema({
+const quizSchema = new Schema({
   title: String,
   topic: Array,
   questions: Array,
   responses: Array
 })
 
-let responseSchema = new Schema({
+const Quizzes = mongoose.model('Quizzes', quizSchema);
+Quizzes.create({title: 'Quiz1'});
+
+const responseSchema = new Schema({
   username: String,
   question: String,
-  response: Mixed
+  response: Array
 })
 
-let userProfileSchema = new Schema({
+const userProfileSchema = new Schema({
   username: String,
   cumulative_score: Number,
   handRaiseCount: Number

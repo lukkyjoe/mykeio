06bbcb1207733f1f8941ed7ccf9a59291744e850
Joseph <joseph.sc.luk@gmail.com>
2017-02-10 19:15:25 -0800

    Create database schemas.

--- db/index.js
+++ db/index.js
@@ -1,1 +1,22 @@
 //Inilialize database connection
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let quizes = new Schema({
+  title: String,
+  topic: Array,
+  questions: Array,
+  responses: 
+})
+
+let responses = new Schema({
+  username: String,
+  question: String,
+  response: Mixed
+})
+
+let userProfile = new Schema({
+  username: String,
+  cumulative_score: Number,
+  handRaiseCount: Number
+})

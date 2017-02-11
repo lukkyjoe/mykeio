abc027a4cb2e83e1e144c5e600eb97f1bb2aa50c
Joseph <joseph.sc.luk@gmail.com>
2017-02-11 13:49:58 -0800

    Build quiz controllers skeleton.

---
+++ db/quizController.js
@@ -0,0 +1,30 @@
+const qa = require('./index.js');
+
+module.exports.createQuiz = function(req, res) {
+  //create a quiz
+};
+
+module.exports.createQuestion = function(req, res) {
+  //create question for quiz
+};
+
+module.exports.deleteQuestion = function(req, res) {
+  //delete a question
+};
+
+module.exports.createResponse = function(req, res) {
+  //create a response
+};
+
+module.exports.createScore = function(req, res) {
+  //create a score for a quiz
+};
+
+
+module.exports.updateScore = function(req, res) {
+  //update a score for a quiz
+};
+
+module.exports.createQuestion = function(req, res) {
+  //create question for quiz
+};

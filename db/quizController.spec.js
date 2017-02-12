568feb89c60d21cd2577cdb93ec242a5dff6d49e
Joseph <joseph.sc.luk@gmail.com>
2017-02-11 17:07:13 -0800

    Pseudocode create and delete steps.

--- db/quizController.spec.js
+++ db/quizController.spec.js
@@ -3,5 +3,9 @@ import quizController from './quizController.js';
 describe('The quiz controller', () => {
     it ('should add a question to an existing quiz successfully', () => {
       console.log("no written yet");
+      //write expectatation
     })
 })
+
+//create and also delete
+

ae32db5ad6fa9799a831a45e11c0ce411d8f098e
Joseph <joseph.sc.luk@gmail.com>
2017-02-11 16:51:01 -0800

    Resolve conflict.

--- db/quizController.js
+++ db/quizController.js
@@ -150,9 +150,7 @@ db.quizzes.update({title: 'time complexity quiz'}, {$push: {questions: "q3"}},
   function(err, model) {
     console.error(err);
     console.log(model);
-<<<<<<< ea008fefcb0155671a90d1d58f7418680a21b2da
-  });
+  }
+);
 
-=======
-  });
->>>>>>> Build more db quiz controllers.
+

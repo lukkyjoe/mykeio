2330f65c994e86fccdde95192914727d4810a280
Joseph <joseph.sc.luk@gmail.com>
2017-02-11 16:51:42 -0800

    Begin quiz controller spec.

---
+++ db/quizController.spec.js
@@ -0,0 +1,7 @@
+import quizController from './quizController.js';
+
+describe('The quiz controller', () => {
+    it ('should add a question to an existing quiz successfully', () => {
+      console.log("no written yet");
+    })
+})

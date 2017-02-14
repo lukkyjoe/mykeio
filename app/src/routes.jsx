85461bafbd962bbe689137f21e0c5e0a23ab6ec8
Joseph <joseph.sc.luk@gmail.com>
2017-02-14 10:30:54 -0800

    Tweak with routes for testing.

--- app/src/routes.jsx
+++ app/src/routes.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import Main from './main.jsx';
 import Room from './room.jsx';
+//remove SessionEditor when not testing
+import SessionEditor from './host/SessionEditor.jsx';
 import {render} from 'react-dom';
 import {Router, Route, Link, hashHistory} from 'react-router'; 
 
+//change route path '/' to point back to {Main}
 render(
   <Router history = {hashHistory}>
     <Route path='/' component={Main}/>

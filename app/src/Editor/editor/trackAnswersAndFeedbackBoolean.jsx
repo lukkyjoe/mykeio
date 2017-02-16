50f3866cebaab076b65f822f8b180b06aa11e112
Joseph <joseph.sc.luk@gmail.com>
2017-02-16 14:44:37 -0800

    Create trackAnswer component.

---
+++ app/src/Editor/editor/trackAnswersAndFeedbackBoolean.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class TrackAnswersAndFeedbackBoolean extends React.Component {
+
+  handleClick(event) {
+    console.log(event);
+    console.log(this.props);
+    this.props.toggleTrackAnswerStatus(event.currentTarget.value);
+  }
+  render() {
+    return (
+      <div>
+        <h3>Track answers? {this.props.trackAnswers}</h3>
+        <form>
+          <div>
+            <label>
+              <input type="radio" name="foo" value={true} onChange={this.handleClick.bind(this)} />
+              Track answers
+            </label>
+          </div>
+          <div>
+            <label>
+              <input type="radio" name="foo" value={false} onChange={this.handleClick.bind(this)}/>
+              Do NOT track answers
+            </label>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default TrackAnswersAndFeedbackBoolean;

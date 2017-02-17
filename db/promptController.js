389fd034eda45a8bb150890a226da164b3dec64d
Joseph <joseph.sc.luk@gmail.com>
2017-02-17 14:58:49 -0800

    Add another test.

--- db/promptController.js
+++ db/promptController.js
@@ -2,7 +2,7 @@ const db = require('./promptSchema.js');
 
 db.HostRoomData.create(
   {
-    roomTitle: 'loomNumbaToo',
+    roomTitle: 'loomNumbaSISIG',
     prompts: [
       {
         responseType: 'MULTIPLE_CHOICE',
@@ -17,6 +17,28 @@ db.HostRoomData.create(
           {
             choiceText: 'Federer',
             correctAnswer: true
+          }, 
+          { choiceText: 'Bolt',
+            correctAnswer: 23
+          }
+        ]
+      },
+      {
+        responseType: 'MULTIPLE_CHOICE',
+        promptText: 'Who is the greatest musician?',
+        trackAnswers: false,
+        giveFeedback: false,
+        options: [
+          {
+            choiceText: 'Muse',
+            correctAnswer: true
+          }, 
+          {
+            choiceText: 'Muse',
+            correctAnswer: true
+          }, 
+          { choiceText: 'Coldplay',
+            correctAnswer: 23
           }
         ]
       }

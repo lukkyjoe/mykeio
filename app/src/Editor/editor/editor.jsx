9565efe835838db06e4167075ecbcda940866deb
Joseph <joseph.sc.luk@gmail.com>
2017-02-16 20:25:56 -0800

    Build update prompt count.

--- app/src/Editor/editor/editor.jsx
+++ app/src/Editor/editor/editor.jsx
@@ -3,18 +3,30 @@ import ReactDOM from 'react-dom';
 import Prompt from './prompt.jsx';
 import PromptCount from './promptCount.jsx'
 
+const testNumber = 7;
+
 class Editor extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      questions: []
+      promptCount : 0,
+      prompts: []
     }
+  }
+  updatePromptNumber(numberOfPrompts) {
+    this.state.promptCount = numberOfPrompts;
+    this.setState({promptCount: numberOfPrompts});
+    console.log(this.state.promptCount);
+  }
+
+  populatePrompts(){
+
   }
 
   render() {
     return (
       <div>
-        <PromptCount /> 
+        <PromptCount updatePromptNumber={this.updatePromptNumber.bind(this)} /> 
         <h2>Settings</h2>    
           <Prompt />                     
       </div>
--- app/src/Editor/editor/promptCount.jsx
+++ app/src/Editor/editor/promptCount.jsx
@@ -7,16 +7,19 @@ class PromptCount extends Component {
       promptNumber: 0
     }
 
-    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({promptNumber: event.target.value});
-  }
+  // handleChange(event) {
+  //   this.setState({promptNumber: event.target.value});
+  //   console.log(this.state.promptNumber)
+  //   console.log(this.props.testNumber)
+  // }
 
   handleSubmit(event) {
     event.preventDefault();
+    console.log('hanldesubmit', this.refs.promptCount.value);
+    this.props.updatePromptNumber(this.refs.promptCount.value);
 
   }
   render() {
@@ -25,9 +28,9 @@ class PromptCount extends Component {
         <form onSubmit={this.handleSubmit}>
           <label>
             How Many Prompts?:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
+            <input type="text" value={this.state.value} ref="promptCount" onChange={this.handleChange} />
           </label>
-          <input type="submit" value="Submit" />
+          <input type="submit" value="Submit"/>
         </form>
       </div>
     );

2ccb0b7a0c42c58d4dabd0dd8a1655e1ebbe164e
Sawyer Schumacher <sawyer.schumacher@gmail.com>
2017-02-18 11:57:22 -0800

    add dev servers

---
+++ dev-index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const path = require('path');
+const process = require('process');
+const apiRoutes = require('./routes/api.js');
+const ExpressPeerServer = require('peer').ExpressPeerServer;
+const app = express();
+
+app.use(session({
+  secret: 'keyboard super cat',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use('/api', apiRoutes);
+
+app.use(express.static(path.join(__dirname, 'app/dist/')));
+app.listen(process.env.PORT || 3100);

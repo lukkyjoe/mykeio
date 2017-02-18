776d2d5418b797d5cf973078d8c16a54d605eec5
Joseph <joseph.sc.luk@gmail.com>
2017-02-18 14:06:08 -0800

    Change update prompt to incrementally add a prompt.

--- app/src/Editor/editor/editor.jsx
+++ app/src/Editor/editor/editor.jsx
@@ -4,27 +4,37 @@ import ReactDOM from 'react-dom';
 import Prompt from './prompt.jsx';
 import PromptCount from './promptCount.jsx';
 
-const testNumber = 7;
-
 class Editor extends React.Component {
   constructor(props) {
     super(props);
     
     this.state = {
+      promptTemplate: {
+      promptText: 'Here is a scary example question',
+      responseType: 'none',
+      choices: [{choice: 'dummy choice', correctAnswer: false}],
+      trackAnswers: false,
+      giveFeedback: false
+    },
       prompts: []
     };
   }
 
   renderPrompts() {
-    var saved = [];
-    for (var i = 0; i < this.state.numberOfPrompts; i++) {
-      saved.push(<Prompt key={i}/>);
-    }
-    return saved;
+    // var saved = [];
+    // for (var i = 0; i < this.state.numberOfPrompts; i++) {
+    //   saved.push(<Prompt key={i}/>);
+    // }
+    // return saved;
   }
 
+// if change to +1 only button, consider the concat option from http://stackoverflow.com/questions/26253351/correct-modification-of-state-arrays-in-reactjs
   updatePromptNumber(numberOfPrompts) {
-    this.setState({numberOfPrompts});
+    console.log('adding one more prompt with misnamed handler')
+      let newArray = this.state.prompts.slice();
+      newArray.push(this.state.promptTemplate);
+      this.setState({prompts: newArray}) 
+    // this.setState({numberOfPrompts});
   }
   
   render() {

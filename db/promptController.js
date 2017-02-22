eb08ce367a05f8c6642b0e328b62abcb2e87cd96
JonathanSchwob <jonathanrschwob@gmail.com>
2017-02-22 15:59:33 -0800

    remove more comments

--- db/promptController.js
+++ db/promptController.js
@@ -45,33 +45,10 @@ db.HostRoomData.create(
     ]
   }, 
   function(err, model) {
-    if (err) {console.error(err);}
-    else {
+    if (err) { 
+      console.error(err);
+    } else {
       console.log(model);
     }
   }
 );
-//test create a quiz
-// db.quizzes.create({
-//     title: "time complexity quiz",
-//     questions: ["q1", "q2"]});
-
-//test add question to existing quiz
-// db.quizzes.update({title: 'time complexity quiz'}, {$push: {questions: "q3"}}, 
-//   {safe: true, upsert: true, new : true},
-//   function(err, model) {
-//     console.error(err);
-//     console.log(model);
-//   }
-// );
-
-//test create entry
-// db.questionAndAnswer.create(
-//   {question: "this is question 3", answer: "this is answer 3"}, 
-//   function(err, model) {
-//     if (err) {console.error(err);}
-//     else {
-//       console.log(model);
-//     }
-//   }
-// )

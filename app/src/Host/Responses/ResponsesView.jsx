a16f3bbac7617f6e89fdb71cece400a43e6277fc
Joseph <joseph.sc.luk@gmail.com>
2017-02-22 11:07:14 -0800

    Change display data to variable.

--- app/src/Host/Responses/ResponsesView.jsx
+++ app/src/Host/Responses/ResponsesView.jsx
@@ -2,18 +2,13 @@ import React from 'react';
 import ResponseTally from './ResponseTally.jsx';
 import styles from './ResponsesView.css';
 
-const fakeResponseData = [
-  {choice: 'hey', tally: 3}, 
-  {choice: 'hello', tally: 7},
-  {choice: 'sup', tally: 3},
-  {choice: 'yo', tally: 4}
-  ]
+
 
 // 1. render different multiple choices and tallies based on selected prompt (from right column)
 // 2. up the tally based on a selection on client side (assume client clicks and selects one)
 
-const ResponsesView = (props) => {
-  const responseList = fakeResponseData.map((response, index) => {
+const ResponsesView = ({displayData}) => {
+  const responseList = displayData.map((response, index) => {
     return (
       <ResponseTally response={response} key={index}/>
     )

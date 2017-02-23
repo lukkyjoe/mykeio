7469ae58cfc54c01ba31bc24d55d2d773e4e8a82
Sawyer Schumacher <sawyer.schumacher@gmail.com>
2017-02-22 16:43:52 -0800

    start landing page

--- app/src/Home.jsx
+++ app/src/Home.jsx
@@ -14,23 +14,32 @@ class Home extends Component {
   handleChange(event) {
     this.setState({roomName: event.target.value});
   }
-  
+
   handleSubmit(event) {
     event.preventDefault();
     window.location.href = '/#/' + this.state.roomName;
   }
- 
+
   render() {
     return (
       <div> 
-        <div className={styles.base}>
-          <a href='/#/editor'>Create Room</a>
+        <div className={styles.dev}>
+          <div className={styles.base}>
+            <a href='/#/editor'>Create Room</a>
+          </div>
+          <div>
+            <form onSubmit={this.handleSubmit}>
+              <input type='text' value={this.state.value} onChange={this.handleChange} />
+              <input type="submit" value="Join Room" />
+            </form>
+          </div>
         </div>
-        <div>
-          <form onSubmit={this.handleSubmit}>
-            <input type='text' value={this.state.value} onChange={this.handleChange} />
-            <input type="submit" value="Join Room" />
-          </form>
+        <div className={styles.splashMain}>
+          <div className={styles.headerMain}>
+            <h2 className={styles.logo}>myke.io</h2>
+            <h3 className={styles.subhead}>Fast, realtime feedback engine for teachers and presenters</h3>
+            <img src='./img/network.svg' className={styles.connectionImage}/>
+          </div>
         </div>
       </div>
     );

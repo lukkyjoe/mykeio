8e23b5e8d0fd556c36327d07c03b0e4d93fd3926
Joseph <joseph.sc.luk@gmail.com>
2017-03-02 11:52:08 -0800

    Draw from props instead of state.

--- app/src/Editor/editor/multipleChoice/multipleChoiceBuilder.jsx
+++ app/src/Editor/editor/multipleChoice/multipleChoiceBuilder.jsx
@@ -5,16 +5,13 @@ import CreateItem from './createItem.jsx';
 export default class MultipleChoiceBuilder extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      choices: this.props.choices
-    };
   }
   render() {
     return (
       <div>
         <CreateItem createChoice={this.props.createChoice}/>
         <ChoicesList 
-          choices={this.state.choices}
+          choices={this.props.choices}
           createChoice={this.props.createChoice}
           saveChoice={this.props.saveChoice}
           deleteChoice={this.props.deleteChoice}
